b03996c7d681fec6bb3fa0c2dd5e4c5bc2527e0b
michael-dwyer-sos-wa-gov <michael.dwyer@sos.wa.gov>
2017-07-11 12:52:39 -0700

    unit tests for RelayCommand

---
+++ Source/BatchThat.Test/RelayCommandTest.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace BatchThat.Test
+{
+    public class RelayCommandTest
+    {
+        private bool MethodCalled;
+        private void Method(object parameter)
+        {
+            MethodCalled = true;
+        }
+
+        [Fact]
+        public void Execute_WhenParameterIsNotNull()
+        {
+            RelayCommand relayCommand = new RelayCommand(Method);
+            Assert.False(MethodCalled);
+
+            relayCommand.Execute("");
+
+            Assert.True(MethodCalled);
+        }
+
+        [Fact]
+        public void Execute_WhenParameterIsNull()
+        {
+            RelayCommand relayCommand = new RelayCommand(Method);
+            Assert.False(MethodCalled);
+
+            relayCommand.Execute(null);
+
+            Assert.False(MethodCalled);
+        }
+
+        [Fact]
+        public void CanExecute()
+        {
+            RelayCommand relayCommand = new RelayCommand(Method);
+
+            bool canExecute = relayCommand.CanExecute("");
+
+            Assert.True(canExecute);
+        }
+    }
+}
