24266c5a417999109f80b2c3ec7f3d5d6b1db2b3
michael-dwyer-sos-wa-gov <michael.dwyer@sos.wa.gov>
2017-07-11 10:59:37 -0700

    Adding unit tests for BatchThat.Image

---
+++ Source/BatchThat.Image.Test/Filters/ResizeTest.cs
@@ -0,0 +1,18 @@
+﻿using BatchThat.Image.Filters;
+using Xunit;
+
+namespace BatchThat.Image.Test.Filters
+{
+    public class ResizeTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            Resize resize = new Resize(10, 20, true);
+
+            Assert.Equal(10, resize.Width);
+            Assert.Equal(20, resize.Height);
+            Assert.True(resize.IgnoreAspectRatio);
+        }
+    }
+}
