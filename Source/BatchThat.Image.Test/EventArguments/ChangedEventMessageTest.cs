724e026013a7e3db72d8c97c0a337f537a36c07d
michael-dwyer-sos-wa-gov <michael.dwyer@sos.wa.gov>
2017-07-11 13:02:34 -0700

    unit tests for ChangedEventMessage

---
+++ Source/BatchThat.Image.Test/EventArguments/ChangedEventMessageTest.cs
@@ -0,0 +1,18 @@
+﻿using BatchThat.Image.Enums;
+using BatchThat.Image.EventArguments;
+using Xunit;
+
+namespace BatchThat.Image.Test.EventArguments
+{
+    public class ChangedEventMessageTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            ChangedEventMessage changedEventMessage = new ChangedEventMessage("some", EnumMessageType.Informational);
+
+            Assert.Equal("some", changedEventMessage.Message);
+            Assert.True(changedEventMessage.MessageType == EnumMessageType.Informational);
+        }
+    }
+}
