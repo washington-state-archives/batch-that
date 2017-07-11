5c469e045815cb9940ad3dc6d6cad757c620ff91
michael-dwyer-sos-wa-gov <michael.dwyer@sos.wa.gov>
2017-07-11 12:19:54 -0700

    unit tests for ViewModelBase

--- Source/BatchThat.Test/ViewModels/ConcreteImageProcessorViewModel.cs
+++ Source/BatchThat.Test/ViewModels/ConcreteImageProcessorViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 using BatchThat.Image.Filters;
 using BatchThat.ViewModels;
 
@@ -6,9 +9,16 @@ namespace BatchThat.Test.ViewModels
 {
     public class ConcreteImageProcessorViewModel : ImageProcessorViewModel
     {
+        public string Field;
+
         public new IList<Filter> GetFilters()
         {
             return base.GetFilters();
+        }
+
+        public new PropertyInfo GetPropertyInfo<TSource, TProperty>(TSource source, Expression<Func<TSource, TProperty>> propertyLambda)
+        {
+            return base.GetPropertyInfo(source, propertyLambda);
         }
     }
 }
---
+++ Source/BatchThat.Test/ViewModels/ViewModelBaseTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace BatchThat.Test.ViewModels
+{
+    public class ViewModelBaseTest
+    {
+        [Fact]
+        public void GetPropertyInfo_WhenParameterIsMethod()
+        {
+            ConcreteImageProcessorViewModel viewModel = new ConcreteImageProcessorViewModel();
+
+            try
+            {
+                viewModel.GetPropertyInfo(viewModel, model => model.GetFilters());
+                Assert.False(true, "exception should have been thrown");
+            }
+            catch (ArgumentException exception)
+            {
+                Assert.Equal("Expression 'model => model.GetFilters()' refers to a method, not a property.",
+                             exception.Message);
+            }
+        }
+
+        [Fact]
+        public void GetPropertyInfo_WhenParameterIsField()
+        {
+            ConcreteImageProcessorViewModel viewModel = new ConcreteImageProcessorViewModel();
+
+            try
+            {
+                viewModel.GetPropertyInfo(viewModel, model => model.Field);
+                Assert.False(true, "exception should have been thrown");
+            }
+            catch (ArgumentException exception)
+            {
+                Assert.Equal("Expression 'model => model.Field' refers to a field, not a property.",
+                             exception.Message);
+            }
+        }
+
+        [Fact]
+        public void GetPropertyInfo()
+        {
+            ConcreteImageProcessorViewModel viewModel = new ConcreteImageProcessorViewModel();
+
+            PropertyInfo propertyInfo = viewModel.GetPropertyInfo(viewModel, model => model.Enabled);
+
+            Assert.Equal(typeof(bool), propertyInfo.PropertyType);
+        }
+    }
+}
